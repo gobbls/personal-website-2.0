65035cc12af145df01858f95a11fd16f67d68e4d
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2025-02-27 18:34:15 +0100

    added TODO item to fix

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   FIX:
   - Fix offcanvas not closing after clicking on route / link.
   - Fix project icons 'reloading' when getting back from a different tab / window.
+  - Fix Chrome issues with project icons being clickable after focused, but not visible.
   TODO:
   - Add back-button and 'current-page / path' -name beside said button for navigation hint.
 -->
